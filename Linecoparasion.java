4981205032c4d43d39a1f2b788007c3050c182ee
vishal sam <vinishakr9@gmail.com>
2022-01-21 18:39:46 +0530

    cheching wether two lines are equal

--- Linecoparasion.java
+++ Linecoparasion.java
@@ -7,10 +7,18 @@ public class Linecoparasion {
 		System.out.println("Wlecome to line copmarasion program");
 		int x1 = 5;
 		int x2 = 10;
+		int x3 = 5;
+		int x4 = 2;
 		int y1 = 5;
-		int y2 = 2;
-		int Line = ((((x2 -x1)*(x2-x1))+((y2-y1)+(y2-y1))));
-		System.out.println(Line);
-
+		int y2 = 10;
+		int y3 = 5;
+		int y4 = 2;
+		int Line_One = ((((x2 -x1)*(x2-x1))+((y2-y1)+(y2-y1))));
+		int Line_Two = ((((x4 -x3)*(x4-x3))+((y4-y3)+(y4-y3))));
+		String Length_One = String.valueOf(Line_One);
+		String Length_Two = String.valueOf(Line_Two);
+		System.out.println(Line_One);
+		System.out.println(Line_Two);
+		System.out.println(Length_One.equals(Length_Two));
 	}
 }
