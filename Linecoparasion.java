8b4f35c7683fb0ffdd15fef062901c513cce6af1
vishal sam <vinishakr9@gmail.com>
2022-01-21 18:29:16 +0530

    added the length of a line

---
+++ Linecoparasion.java
@@ -0,0 +1,16 @@
+package com.linecoparasion;
+
+public class Linecoparasion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wlecome to line copmarasion program");
+		int x1 = 5;
+		int x2 = 10;
+		int y1 = 5;
+		int y2 = 2;
+		int Line = ((((x2 -x1)*(x2-x1))+((y2-y1)+(y2-y1))));
+		System.out.println(Line);
+
+	}
+}
